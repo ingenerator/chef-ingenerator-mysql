b1a64db614d3a1848524852b89c13d5f1257daea
Andrew Coulton <andrew@ingenerator.com>
2017-08-09 16:23:52 +0100

    Update to 0.4.0 and list ingenerator chef-supermarket

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description 'Standard mySQL installation for our applications, including relevan
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/ingenerator/chef-ingenerator-mysql/issues'
 source_url 'https://github.com/ingenerator/chef-ingenerator-mysql'
-version '0.3.0'
+version '0.4.0'
 
 %w(ubuntu).each do |os|
   supports os
