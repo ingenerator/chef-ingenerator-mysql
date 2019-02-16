5ed0c2eaafbd3e9ab4ae8e2a4b9137ab4fdce32d
acoulton <andrew@ingenerator.com>
2019-02-16 00:04:03 +0000

    Drop chef12 support, require Chef13

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name 'ingenerator-mysql'
 maintainer 'Andrew Coulton'
 maintainer_email 'andrew@ingenerator.com'
 license 'Apache-2.0'
-chef_version '>=12.18.31'
+chef_version '>=13.12.1'
 description 'Standard mySQL installation for our applications, including relevant PHP and application config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/ingenerator/chef-ingenerator-mysql/issues'
 source_url 'https://github.com/ingenerator/chef-ingenerator-mysql'
-version '0.4.1'
+version '0.5.0'
 
 %w(ubuntu).each do |os|
   supports os
